ab4a4a668dc28b1075db9653c8355088beac7118
kasun_vimu <kasunvimu8@gmail.com>
2021-08-21 23:25:03 +0530

    Empty card handling
    Empty card handling

--- src/components/common/ShoppingCart.js
+++ src/components/common/ShoppingCart.js
@@ -59,123 +59,118 @@ export const ShoppingCart = (props) => {
   return (
     <div className="h-100">
       {/* Empty Screen */}
-      <div className="h-50 d-flex align-items-center justify-content-center">
-        <Empty>
-          <i class="ti ti-shopping-cart"></i>
-          <p>Your cart is empty...</p>
-        </Empty>
-      </div>
-
-      {/* Cart Screen */}
-      <div className="panel-cart-container">
-        <div className="panel-cart-content cart-details">
-          {cartItems?.length !== 0 ? (
-            <>
-              <table className="cart-table">
-                <tbody>
-                  {cartItems.map((cart) => {
-                    return (
-                      <tr key={cart.id}>
-                        <td className="title">
-                          <span className="name">
-                            <a href="#product-modal" data-toggle="modal">
-                              {cart.product.name}
-                            </a>
-                          </span>
-                          {cart.product?.menu_option_categories?.map(
-                            (category) => {
-                              return (
-                                category?.selectOption && (
-                                  <span
-                                    className="caption text-muted"
-                                    key={category?.selectOption.id}
-                                  >
-                                    {category?.selectOption.name}&nbsp;
-                                  </span>
-                                  // <span className="caption text-muted">Large (500g)</span>
-                                )
-                              );
-                            }
-                          )}
-                        </td>
-                        <td>{cart.quantity}</td>
-                        <td className="price">${cart.cost.toFixed(2)}</td>
-                        <td className="actions">
-                          <a
-                            href="#product-modal"
-                            data-toggle="modal"
-                            className="action-icon"
-                          >
-                            <i
-                              className="ti ti-pencil"
-                              onClick={() => onClickEdit(cart)}
-                            ></i>
-                          </a>
-                          &nbsp;
-                          <a href="#" className="action-icon">
-                            <i
-                              className="ti ti-close"
-                              onClick={() => onClickClose(cart)}
-                            ></i>
-                          </a>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-
-              <div className="cart-summary">
-                <div className="row">
-                  <div className="col-7 text-right text-muted">
-                    Order total:
-                  </div>
-                  <div className="col-5">
-                    <strong>
-                      $
-                      <span className="cart-products-total">
-                        {totalCost.toFixed(2)}
-                      </span>
-                    </strong>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-7 text-right text-muted">Devliery:</div>
-                  <div className="col-5">
-                    <strong>
-                      $
-                      <span className="cart-delivery">
-                        {deliveryCost.toFixed(2)}
-                      </span>
-                    </strong>
-                  </div>
-                </div>
-                <hr className="hr-sm" />
-                <div className="row text-lg">
-                  <div className="col-7 text-right text-muted">Total:</div>
-                  <div className="col-5">
-                    <strong>
-                      $
-                      <span className="cart-total">{finalCost.toFixed(2)}</span>
-                    </strong>
-                  </div>
-                </div>
-              </div>
-            </>
-          ) : (
-            <div className="cart-empty">
-              <i className="ti ti-shopping-cart"></i>
-              <p>Your cart is empty...</p>
-            </div>
-          )}
+      { cartItems?.length === 0 ? (
+        <div className="h-50 d-flex align-items-center justify-content-center">
+          <Empty>
+            <i className="ti ti-shopping-cart"></i>
+            <p>Your cart is empty...</p>
+          </Empty>
         </div>
+      ) : (
+        <div>
+        {/* Cart Screen */}
+        <div className="panel-cart-container">
+          <div className="panel-cart-content cart-details">
+            <table className="cart-table">
+              <tbody>
+                {cartItems.map((cart) => {
+                  return (
+                    <tr key={cart.id}>
+                      <td className="title">
+                        <span className="name">
+                          <a href="#product-modal" data-toggle="modal">
+                            {cart.product.name}
+                          </a>
+                        </span>
+                        {cart.product?.menu_option_categories?.map(
+                          (category) => {
+                            return (
+                              category?.selectOption && (
+                                <span
+                                  className="caption text-muted"
+                                  key={category?.selectOption.id}
+                                >
+                                  {category?.selectOption.name}&nbsp;
+                                </span>
+                                // <span className="caption text-muted">Large (500g)</span>
+                              )
+                            );
+                          }
+                        )}
+                      </td>
+                      <td>{cart.quantity}</td>
+                      <td className="price">${cart.cost.toFixed(2)}</td>
+                      <td className="actions">
+                        <a
+                          href="#product-modal"
+                          data-toggle="modal"
+                          className="action-icon"
+                        >
+                          <i
+                            className="ti ti-pencil"
+                            onClick={() => onClickEdit(cart)}
+                          ></i>
+                        </a>
+                        &nbsp;
+                        <a href="#" className="action-icon">
+                          <i
+                            className="ti ti-close"
+                            onClick={() => onClickClose(cart)}
+                          ></i>
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+
+            <div className="cart-summary">
+                  <div className="row">
+                    <div className="col-7 text-right text-muted">
+                      Order total:
+                    </div>
+                    <div className="col-5">
+                      <strong>
+                        $
+                        <span className="cart-products-total">
+                          {totalCost.toFixed(2)}
+                        </span>
+                      </strong>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-7 text-right text-muted">Devliery:</div>
+                    <div className="col-5">
+                      <strong>
+                        $
+                        <span className="cart-delivery">
+                          {deliveryCost.toFixed(2)}
+                        </span>
+                      </strong>
+                    </div>
+                  </div>
+                  <hr className="hr-sm" />
+                  <div className="row text-lg">
+                    <div className="col-7 text-right text-muted">Total:</div>
+                    <div className="col-5">
+                      <strong>
+                        $
+                        <span className="cart-total">{finalCost.toFixed(2)}</span>
+                      </strong>
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+        <Button
+          className="panel-cart-action btn btn-secondary btn-block btn-lg"
+          onClick={handleCheckout}
+        >
+          <span>Go to checkout</span>
+        </Button>
       </div>
-      <Button
-        className="panel-cart-action btn btn-secondary btn-block btn-lg"
-        onClick={handleCheckout}
-      >
-        <span>Go to checkout</span>
-      </Button>
+      )}
     </div>
   );
 };
